175075ee6b1d60fbf449e1430eccb107ad9a0bdf
pyraq <pyraq@naver.com>
2019-12-28 03:55:47 +0900

    [Sort Practice]  1. bubble sort

---
+++ SortExample/SortExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SortLibrary;
+
+namespace SortExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new MySortedList<int>() { 8, 9, 2, 30, 81, -4, 847, -1028, 284, 12, -7, -4, 81 };
+
+            list.BubbleSort();
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+}
---
+++ SortExample/SortLibrary/MySortedList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortLibrary
+{
+    public class MySortedList<T> : List<T> where T : IComparable
+    {
+        public void BubbleSort()
+        {
+            var len = this.Count;
+
+            for (int i = 0; i < len - 1; i++)
+            {
+                for (int j = 0; j < len - 1 - i; j++)
+                {
+                    var f = this[j];
+                    var s = this[j + 1];
+                    if (f.CompareTo(s) == 1)
+                    {
+                        this[j] = s;
+                        this[j + 1] = f;
+                    }
+                }
+            }
+        }
+    }
+}
